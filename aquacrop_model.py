50036f2d2a35aca5f1f7a59046b451f9a54b1933
Jintsp <jint_pongchababnapa@yahoo.com>
2025-05-13 18:01:08 +0100

    Initial commit

---
+++ aquacrop_model.py
@@ -0,0 +1,6 @@
+from aquacrop import AquaCropModel, Soil, Crop, InitialWaterContent, FieldMngt, GroundWater
+from aquacrop.utils import prepare_weather, get_filepath
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
